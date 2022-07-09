bd24a2fcf094a1dbf0cf0a8c5bd98b19231bc770
giovanavi <giovana.gvi02@gmail.com>
2022-07-08 21:05:14 -0300

    update datepicker

--- src/components/DatePicker.vue
+++ src/components/DatePicker.vue
@@ -6,14 +6,79 @@ export default {
             myTime: null,
             myDate: null,
         }
-    },
-    methods: {
-
     }
 }
 </script>
 
 <template>
+
+<div>
+    <div class="overlay">
+        <div class="modal">
+            <div class="flex items-center justify-bettween p-3">
+                <span> Modal Title </span>
+                
+                <button>X</button>
+            </div>
+        </div>
+        <div class="p-3 bg-dark">
+            <div class="mb-4"> 
+                <p>Marcar a Consulta</p> 
+            </div>
+                <form class="">
+                    <div id="date-picker" inline="true">
+                        <label for="example">Escolha a data para a consulta</label>
+                        <input v-model="myDate" placeholder="Select date" type="date" id="example" class="form-control">
+                    </div>
+                    <div class="row">
+                        <div id="time-picker" class="col-mb-2" inline="true">
+                            <label for="example">Escolha o horário consulta</label>
+                            <input v-model="myDate" placeholder="Select date" type="time" id="example" class="form-control">
+                        </div>
+                    </div>
+                </form>
+            <button class="p-2 text-sm rounded">
+                Confirm
+            </button>
+            <button class="p-2 text-sm rounded">
+                Exit
+            </button>
+        </div>
+    </div>
+
+</div>
+
+</template>
+
+<style scoped>
+label{
+    color: white;
+}
+p{
+    color: white;
+}
+.overlay{
+    position: fixed;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    background-color: rgba(0,0,0,0.3);
+    z-index: 100;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+.modal{
+    width: 500px;
+    box-shadow: 1px 2px 4px rgba(153, 155, 168, .12);
+    border-radius: 4px;
+}
+
+</style>
+
+    <!--
     <div class="card" style="width: 20rem;">
         <div class="card-body">
             <h5 class="card-title">Marcar Consulta</h5>
@@ -26,15 +91,13 @@ export default {
                 </div>
                 <div class="row">
                     <div id="time-picker" class="col-mb-2" inline="true">
-                        <label for="example">Escolha a data para a consulta</label>
+                        <label for="example">Escolha o horário consulta</label>
                         <input v-model="myDate" placeholder="Select date" type="time" id="example" class="form-control">
-                        <h1>{{myDate}}</h1>
+                        <h1>{{myTime}}</h1>
                     </div>
                 </div>
             </form>
         </div>  
     </div>
-</template>
-
-
+    -->
 
--- src/components/DoctorCard.vue
+++ src/components/DoctorCard.vue
@@ -1,17 +1,36 @@
 <script>
+import DatePicker from './DatePicker.vue'
+
 export default {
-    name:"DoctorCard"    
+    name:"DoctorCard",
+    data(){
+      return{
+        isVisible: false
+      }
+    },
+    methods: {
+      toogleModal(){
+        this.isVisible = !this.isVisible
+      }
+    },
+    components:{
+      DatePicker
+    }    
 }
 </script>
 
 <template>
+
 <div class="row">
   <div class="col-sm-6">
     <div class="card">
       <div class="card-body">
         <h5 class="card-title">Special title treatment</h5>
         <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
+            <DatePicker></DatePicker>
+            <button @click.prevent="toogleModal" class="bg-blue-500  p-2 text-sm rounded">
+              Open Modal
+            </button>
       </div>
     </div>
   </div>
@@ -20,9 +39,14 @@ export default {
       <div class="card-body">
         <h5 class="card-title">Special title treatment</h5>
         <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
+            <DatePicker></DatePicker>
+            <button @click.prevent="toogleModal" class="bg-blue-500  p-2 text-sm rounded">
+              Open Modal
+            </button>
       </div>
     </div>
   </div>
 </div>
+
+
 </template>
